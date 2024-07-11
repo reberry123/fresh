815ddebe4d906597864d06a35c89d0d3215231f1
reberry <74585828+reberry123@users.noreply.github.com>
2024-07-12 00:07:12 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,4 +12,17 @@ const nextConfig = {
     assetPrefix: prefix,
 }
 
-module.exports = nextConfig
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
